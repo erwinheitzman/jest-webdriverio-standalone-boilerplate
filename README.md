cd45f29fb3e1162863c747b100d3095a7ba976b9
Mykola Grybyk <mykola.grybyk@gmail.com>
2019-12-06 01:19:37 +0100

    add expect-webdriverio

---
+++ README.md
@@ -0,0 +1,5 @@
+# jest-webdriverio-standalone-boilerplate
+
+1. `npm i`
+2. run chromedriver
+3. run tests `npm test`
--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  setupFilesAfterEnv: ['./jest.setup.ts', './jest.teardown.ts']
+  setupFilesAfterEnv: ['./jest.setup.ts', './jest.teardown.ts', 'expect-webdriverio']
 };
