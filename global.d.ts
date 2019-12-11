61fb5e29fd3d174c69492b13b2d3d52765a3bc79
christian-bromann <mail@christian-bromann.com>
2019-12-10 23:16:55 -0500

    demonstrate sync and async support

--- global.d.ts
+++ global.d.ts
@@ -1,5 +1,19 @@
+declare namespace ChromeDriverLauncher {
+  function onPrepare (
+    config: object
+  ): void;
+  function onComplete (): void;
+}
+
 declare module NodeJS  {
   interface Global {
-      browser: any;
+    browser: any;
+    chromedriverLauncher: ChromeDriverLauncher;
+  }
+}
+
+declare module 'wdio-chromedriver-service' {
+  export {
+    launcher: ChromeDriverLauncher
   }
 }
