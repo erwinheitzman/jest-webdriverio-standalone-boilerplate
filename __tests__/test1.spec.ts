4207457c0c85b883e77cdb19b2a12b4e40bd86f6
Erwin Heitzman <15839059+erwinheitzman@users.noreply.github.com>
2021-07-27 15:32:54 +0200

    updated setup, removed sync example

---
+++ __tests__/test1.spec.ts
@@ -0,0 +1,23 @@
+import { remote, Browser } from "webdriverio";
+import { config } from "../wdio.conf";
+
+let browser: Browser<"async">;
+
+beforeAll(async () => {
+  browser = await remote(config);
+});
+
+afterAll(async () => {
+  await browser.deleteSession();
+});
+
+test("WebdriverIO test 1", async () => {
+  await browser.url("https://webdriver.io");
+  expect(await browser.getTitle()).toContain("WebdriverIO");
+  const searchButton = await browser.$(".DocSearch-Button");
+  await searchButton.click();
+  const searchBar = await browser.$("#docsearch-input");
+  await searchBar.setValue("click");
+  const suggestions = await browser.$(".DocSearch-Hit");
+  await suggestions.waitForExist();
+});
--- __tests__/test2.spec.ts
+++ __tests__/test2.spec.ts
@@ -11,7 +11,7 @@ afterAll(async () => {
   await browser.deleteSession();
 });
 
-test("asynchronous WebdriverIO test", async () => {
+test("WebdriverIO test 2", async () => {
   await browser.url("https://webdriver.io");
   expect(await browser.getTitle()).toContain("WebdriverIO");
   const searchButton = await browser.$(".DocSearch-Button");
