0b8b237864ba108e4b86e4795a1a9bcc16366ddb
Mykola Grybyk <mykola.grybyk@gmail.com>
2023-08-07 11:03:11 +0200

    headless setup and ci

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -1,8 +1,17 @@
 import { RemoteOptions } from "webdriverio";
 
+const args = [];
+const headless = !!process.env.HEADLESS;
+if (headless) {
+  args.push("--headless");
+}
+
 export const config: RemoteOptions = {
   capabilities: {
     browserName: "chrome",
+    "goog:chromeOptions": {
+      args,
+    },
   },
   waitforTimeout: 15000,
   logLevel: "silent",
